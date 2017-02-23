92f084fe160121d83aa296d64e2f5f566e10ce54
Mike Sausa <mikejsausa@gmail.com>
2017-02-23 16:36:01 -0700

    added intro text

--- src/components/App.js
+++ src/components/App.js
@@ -36,7 +36,6 @@ class App extends Component {
         <Work />
         <Testimonials />
         <Blog />
-        <Signature />
       </div>
     );
   }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,8 @@ class Intro extends Component {
   render() {
     return (
       <section id="intro">
-        
+        <h1 id="name">Mike Sausa</h1>
+        <h2 id="title">Front-End Developer</h2>
       </section>
     );
   }
