fd39b9fd10ebd569ce26cc56a698d7450779f333
Mike Sausa <mikejsausa@gmail.com>
2017-02-23 20:23:37 -0700

    added arrow scroll

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-scroll';
 
 class Hero extends Component {
   render() {
@@ -6,6 +7,9 @@ class Hero extends Component {
       <section id="hero">
         <h1 id="name">Mike Sausa</h1>
         <h2 id="title">Front-End Developer</h2>
+        <Link activeClass="active" to="Work" smooth={true} offset={20} duration={1750}>
+          <i className="Arrow" />
+        </Link>
       </section>
     );
   }
