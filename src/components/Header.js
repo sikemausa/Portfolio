65d824452e6bfa616aafdef92040eaeb57a43dac
Mike Sausa <mikejsausa@gmail.com>
2017-02-23 19:08:42 -0700

    cleared house

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Headroom from 'react-headroom';
+import { Link } from 'react-scroll';
 
 class Header extends Component {
   render() {
@@ -7,13 +8,18 @@ class Header extends Component {
       <nav id="header">
         <Headroom>
           <ul className="header-nav">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#overview">Overview</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#competencies">Competencies</a></li>
-            <li><a href="#work">Work</a></li>
-            <li><a href="#testimonials">Testimonials</a></li>
-            <li><a href="#blog">Blog</a></li>
+            <Link activeClass="active" to="Hero" smooth={true} offset={20} duration={1750}>
+              <li>Home</li>
+            </Link>
+            <Link activeClass="active" to="About" smooth={true} offset={20} duration={1750}>
+              <li>About</li>
+            </Link>
+            <Link activeClass="active" to="Work" smooth={true} offset={20} duration={1750}>
+              <li>Work</li>
+            </Link>
+            <Link activeClass="active" to="Blog" smooth={true} offset={20} duration={1750}>
+              <li>Blog</li>
+            </Link>
           </ul>
         </Headroom>
       </nav>
