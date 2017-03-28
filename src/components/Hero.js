fe78f999735bf0cb7c7d9aa03c9c6ab13a969b5e
Mike Sausa <mikejsausa@gmail.com>
2017-03-27 20:59:43 -0600

    added pointer cursor to links

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ class Hero extends Component {
         <h1 id="name">Mike Sausa</h1>
         <h2 id="title">Front-End Developer</h2>
         <Link id="arrow-link" activeClass="active" to="About" smooth={true} duration={1750}>
-          <i className="Arrow" />
+          <i className="Arrow nav-link" />
         </Link>
       </section>
     );
